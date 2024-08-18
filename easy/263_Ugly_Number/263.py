907672d2ee1e01777ad004ea0a224b46393c9f64
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2024-08-18 22:17:26 +0530

    easy 263 ugly numbers

---
+++ easy/263_Ugly_Number/263.py
@@ -0,0 +1,8 @@
+#URL : https://leetcode.com/problems/ugly-number/description/
+#[Easy] [263] 
+#Title: [Ugly Number]
+#Author: Kartik Bhatnagar
+#Date : 2024-08-18 (YYYY-MM-DD)
+
+if __name__ == "__main__":
+    s=Solution()
