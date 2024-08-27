a3b531e5ec094534ebadb8813f6448a988800dca
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2024-08-27 23:40:48 +0530

    27 aug not done

---
+++ Medium/1514_Path_with_Maximum_Probability/1514.py
@@ -0,0 +1,8 @@
+#URL : https://leetcode.com/problems/path-with-maximum-probability/?envType=daily-question&envId=2024-08-27
+#[Medium] [1514] 
+#Title: [Path with Maximum Probability]
+#Author: Kartik Bhatnagar
+#Date : 2024-08-27 (YYYY-MM-DD)
+
+if __name__ == "__main__":
+    s=Solution()
