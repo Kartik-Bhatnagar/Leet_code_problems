63886f53a9f9367952aca8a57ca2fdaa594a1951
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2024-09-12 10:25:53 +0530

    bat file

---
+++ scripts/create.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\Users\kbhatnag\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\kbhatnag\OneDrive - Itron\Python_prorams_self\Leetcode\scripts\create_file_folder.py"
+pause
