6ce5ebe7c340aed7aa58d2340ce1e439cf676c4d
KartikBhatnagar <kartikbhatnagar15@gmail.com>
2024-09-20 15:07:49 +0530

    214 hard

---
+++ Hard/214_Shortest_Palindrome/214.py
@@ -0,0 +1,8 @@
+#URL : https://leetcode.com/problems/shortest-palindrome/description/?envType=daily-question&envId=2024-09-20
+#[Hard] [214] 
+#Title: [Shortest Palindrome]
+#Author: Kartik Bhatnagar
+#Date : 2024-09-20 (YYYY-MM-DD)
+
+if __name__ == "__main__":
+    s=Solution()
